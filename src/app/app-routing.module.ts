5f87c8ce79e283b47051b93de8fdadef05357420
kimaniavery075@gmail.com <kimaniavery075@gmail.com>
2023-01-30 17:06:28 -0700

    Could not create a PFiance module correctly to handle routing, so instead I've created a budget module that is lazy loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BudgetModule } from './budget/budget.module';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'budget', loadChildren: () => import('./budget/budget.module').then(m => m.BudgetModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BudgetModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
